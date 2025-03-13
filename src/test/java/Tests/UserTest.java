4055fd14187e08782e6b81cea1954af19714086b
mac <Thanuragayanaka9884@gmail.com>
2025-03-14 04:59:07 +0530

    part2

--- src/test/java/Tests/UserTest.java
+++ src/test/java/Tests/UserTest.java
@@ -4,25 +4,24 @@ import BaseTest.BaseTest;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
 
 import static io.restassured.RestAssured.given;
 
 public class UserTest extends BaseTest {
 
-
-    @Test
-
+    @Test(dataProvider = "zipCodes", dataProviderClass = data.TestdataProvider.class)
     public void ValidatezipcodeDeatails(String country, String zip, String place){
 
         Response response = given()
                 .when()
-                .get("/us/90210")
+                .get("/" + country + "/" + zip) // Dynamically using country and zip
                 .then()
                 .statusCode(200)
                 .extract().response();
 
         // Assertions - validate place and country
-        Assert.assertEquals(response.jsonPath().getString("places[0].'place name'"), "Beverly Hills");
+        Assert.assertEquals(response.jsonPath().getString("places[0].'place name'"), place);
         Assert.assertEquals(response.jsonPath().getString("country"), "United States");
 
         // Print response for debugging
--- src/test/java/data/TestdataProvider.java
+++ src/test/java/data/TestdataProvider.java
@@ -1,31 +1,16 @@
 package data;
 
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 
 public class TestdataProvider {
 
-    @DataProvider(name ="zipCodes")
-
-
-    public Object [][] getZipCodeData(){
-
+    @DataProvider(name = "zipCodes")
+    public Object[][] getZipCodeData() {
         return new Object[][]{
-
                 {"us", "90210", "Beverly Hills"},
                 {"us", "10001", "New York"},
                 {"ca", "M5S", "Toronto"},
                 {"de", "10115", "Berlin"}
-
-
         };
-
-
-
-
-
-
-
     }
-
 }
